a695d7c53fe42feda1deb4ea746ad578c202125a
wire-ai <wire613@gmail.com>
2022-05-08 09:18:53 +0800

    upload academy travel bali

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        pacifico: ["Pacifico"],
+        sansPro: ["Source Sans Pro"],
+      },
+      colors: {
+        primary: "#FCB371",
+        secondary: "#2A263D",
+      },
+      backgroundImage: {
+        "home-page": "url(/img/imageBackground.png)",
+        "icon-bali": "url(/img/IconBali.png)",
+        "about-page": "url(/img/bg1.png)",
+        reservation: "url(/img/bg2.png)",
+      },
+    },
+  },
+  plugins: [require("@tailwindcss/forms")],
+};
